9d1987e909e82519191b81845231e6b5d0a63130
AudreyChampeau <32376175+AudreyChampeau@users.noreply.github.com>
2017-10-01 22:35:08 -0700

    Add files via upload

--- supermarket_optimization.py
+++ supermarket_optimization.py
@@ -3,21 +3,25 @@
 
 '''
 author : Audrey Champeau
+
+This script provides frequent sets of elements based on a list of transactions.
 '''
 
 import argparse
+import itertools
+import operator
 
 parser = argparse.ArgumentParser(description="Provides frequent sets of elements based on a list of transactions.")
 parser.add_argument('input_file', type=str,
-                   help='the name of the input file')
+                   help='the name of the input file that contains a list of space-separated numbers in each line')
 parser.add_argument('-o', '--output_file', dest='output_file_name', type=str,
                    help='the name of the output file')
-parser.add_argument('-l', '--sets_min_size', dest='sets_min_size', type=int, default=3,
+parser.add_argument('--sets_min_size', dest='sets_min_size', type=int, default=3,
                    help='the minimum size for the sets')
-parser.add_argument('-s', '--support_level', dest='support_level', type=int, default=4,
-                   help='the support level')
-parser.add_argument('-b', '--needs_sorting', dest='needs_sorting', type=bool, default=False,
-                   help='the support level')
+parser.add_argument('--support_level', dest='support_level', type=int, default=4,
+                   help='the support level (or frequency)')
+parser.add_argument('--needs_sorting', action='store_true',
+                   help='if the transactions need to be sorted')
 
 args = parser.parse_args()
 
@@ -30,7 +34,7 @@ needs_to_be_sorted = args.needs_sorting
 output_file_name = "output_size-"+str(min_set_size)+"_freq-"+str(min_frequency)
 if args.output_file_name != None:
 	output_file_name = args.output_file_name
-out = open(output_file_name, "w") # create the file, strings will be appended afterwards
+out = open(output_file_name, "w") # create the file, strings will be appended to it afterwards
 out.close()
 
 max_set_size = 0
@@ -67,54 +71,48 @@ def retrieve_relevant_rows_from_file(file_name):
 	return relevant_rows
 	
 # get all combinations of size s containing element e of index i in row r
-def get_combinations(r, i, s, start=0):
+def get_combinations(r, i, s):
 	combinations = []
-	r_size = len(r)
-	# base cases : 
-	# - if the size of the row is smaller than the set size, no set can be found, return an empty list
-	# - if the starting point is at the end of the row, all sets have been found, return them
-	if (r_size < s or start == r_size-1):
-		return combinations
-	# recursion
-	else:
-		# if the starting point is at the element e, go to the next index
-		if (start == i):
-			return get_combinations(r, i, s, start+1)
-		else:
-			# find all possible sets in the row that contain the element e 
-			elements = r
-			comb = []
-			# start by putting the element e in the set
-			comb.append(r[i])
-			r_index = start
-			# then fill it with other elements, starting by index start
-			while (len(comb) < s):
-				if r_index == i:
-					r_index += 1
-				if r_index >= len(r):
-					r_index = 0
-				comb.append(r[r_index])
-				r_index += 1
-			combinations.append(tuple(comb)) # since the rows are sorted, two tuples containing the same elements will always be ordered the same way
-			# repeat the operation after incrementing the starting point
-			combinations.extend(get_combinations(r, i, s, start+1))
-			return combinations
+	r_size = len(r)  
+	# start by putting aside the element e
+	element = []
+	element.append(r[i])
+	# then get all possible (unordered) combinations of size s-1 of the rest of the elements
+	tmp_row = list(r)
+	tmp_row.remove(r[i])
+	combs = itertools.combinations(tmp_row, s-1)
+	# finally, combine them to get all possible combinations of size s that contain element e
+	for c in combs:
+		set = []
+		set.append(r[i])
+		for e in c:
+			set.append(e)
+		combinations.append(tuple(set))
+	return combinations
 
-# get a dictionary that associates sets of elements containing a specific element to their frequency	
-def get_sets_to_frequency(rows, element, element_to_delete):
+# convert a tuple into a string
+def get_string(tuple):
+	s = ""
+	for t in tuple:
+		s += str(t)+"#"
+	return s[:len(s)-1]
+	
+# get a dictionary that associates sets of elements containing a specific element with their frequency
+def get_sets_to_frequency(rows, element, set_size, previous_element):
 	sets_to_frequency = {}
 	for row in rows:
-		# delete the previous element that has already been processed (all the sets containing the element have already been processed, so the element can be removed from all the rows to save time)
-		if element_to_delete != None and element_to_delete in row:
-			row.remove(element_to_delete)
-		for set_size in range(min_set_size, max_set_size+1):
-			if element in row:
-				i = row.index(element)
-				combinations = get_combinations(row, i, set_size)
-				for combination in combinations:
-					if combination not in sets_to_frequency:
-						sets_to_frequency[combination] = 0
-					sets_to_frequency[combination] += 1
+		# an element that has been entirely processed can be safely removed, all sets containing it have already been found
+		if previous_element in row:
+			row.remove(previous_element)
+		if element in row:
+			i = row.index(element)
+			combinations = get_combinations(row, i, set_size)
+			# as the rows are sorted, sets that contain the same elements will always be ordered the same way
+			for combination in combinations:
+				c = get_string(combination)
+				if c not in sets_to_frequency:
+					sets_to_frequency[c] = 0
+				sets_to_frequency[c] += 1
 	return sets_to_frequency
 	
 # get the sets that have at least the minimum frequency	
@@ -124,7 +122,8 @@ def get_relevant_sets(dic):
 # write in the output file	
 def write_output(file_name, sets_to_frequency):
 	with open(file_name, "a") as file:
-		for s, f in sets_to_frequency.items():
+		for k, f in sets_to_frequency.items():
+			s = tuple(list(int(v) for v in k.split("#")))
 			to_print = str(len(s))+", "+str(f)+", "
 			for e in s:
 				to_print += str(e)+", "
@@ -139,17 +138,25 @@ relevant_rows = retrieve_relevant_rows_from_file(input_file_name)
 
 print ("Processing...")
 total = len(all_unique_elements)
+
+# sort all elements by ascending occurrences so that rare elements are processed first and frequent elements are processed in the end, when the rows contain fewer elements
+sorted_aue = sorted(all_unique_elements.items(), key=operator.itemgetter(1), reverse=True)
+all_unique_elements = []
 counter = 0
-element_to_delete = None
-# Process elements one by one (instead of having a very big data structure in memory)
-for i, element in enumerate(all_unique_elements):
+previous_element = None
+# Process elements one by one (to avoid having a very big data structure in memory)
+for (element, occ) in sorted_aue:
 	counter += 1
-	print(str(counter)+"/"+str(total))
-	sets_to_frequency = get_sets_to_frequency(relevant_rows, element, element_to_delete)
-	relevant_sets_to_frequency = get_relevant_sets(sets_to_frequency)
-	write_output(output_file_name, relevant_sets_to_frequency)
-	# store the value of the element already processed so that it will be ignored in the next loop
-	element_to_delete = element
+	print(str(counter)+"/"+str(total)+" [element="+str(element)+" : occurrences="+str(occ)+"]")
+	# Process entirely each size at a time (again, to avoid having a very big data structure in memory)
+	for set_size in range(min_set_size, max_set_size+1):
+		print("set size : "+str(set_size))
+		sets_to_frequency = get_sets_to_frequency(relevant_rows, element, set_size, previous_element)
+		print("set_to_frequency size : "+str(len(sets_to_frequency)))
+		relevant_sets_to_frequency = get_relevant_sets(sets_to_frequency)
+		write_output(output_file_name, relevant_sets_to_frequency)
+	# store the element that has been processed so that it can be removed of all the rows in the next loop (saving time)
+	previous_element = element
 
 print ("All done.\nRetrieve results in the file '"+output_file_name+"'.")
 
